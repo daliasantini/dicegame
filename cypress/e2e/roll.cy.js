d6cd3031457cdb1c798925645bcd1a0a3bc6435c
daliasantini <santini.dg@hotmail.com>
2025-09-29 09:30:05 -0600

    Add it to show correct message depending on dice rolls

--- cypress/e2e/roll.cy.js
+++ cypress/e2e/roll.cy.js
@@ -3,27 +3,40 @@ describe('Rolling', () => {
     cy.visit('https://dice-game-2d3b5.web.app');
 
     //Store initial dice values
-   cy.getByCy('dice1').invoke('attr', 'src').as('firstSrc1');
-   cy.getByCy('dice2').invoke('attr', 'src').as('firstSrc2');
+    cy.getByCy('dice1').invoke('attr', 'src').as('firstSrc1');
+    cy.getByCy('dice2').invoke('attr', 'src').as('firstSrc2');
 
-   //Click roll button
+    //Click roll button
     cy.getByCy('roll-btn').click();
 
     //Assert dice 1 changed
     cy.get('@firstSrc1').then((firstSrc1) => {
-        cy.getByCy('dice1', {timeout: 5000}).invoke('attr', 'src')
+      cy.getByCy('dice1', { timeout: 5000 })
+        .invoke('attr', 'src')
         .should((newSrc1) => {
-           expect(newSrc1).not.to.eq(firstSrc1);
+          expect(newSrc1).not.to.eq(firstSrc1);
         });
     });
 
     //Assert dice 2 changed
-     cy.get('@firstSrc2').then((firstSrc2) => {
-        cy.getByCy('dice2', {timeout: 5000}).invoke('attr', 'src')
+    cy.get('@firstSrc2').then((firstSrc2) => {
+      cy.getByCy('dice2', { timeout: 5000 })
+        .invoke('attr', 'src')
         .should((newSrc2) => {
-           expect(newSrc2).not.to.eq(firstSrc2);
+          expect(newSrc2).not.to.eq(firstSrc2);
         });
     });
-   
+  });
+
+  it('shows correct message depending on dice rolls', () => {
+    cy.visit('https://dice-game-2d3b5.web.app');
+
+    cy.getByCy('roll-btn').click();
+
+    cy.getByCy('message', { timeout: 5000 })
+      .inovke('text')
+      .then((msg) => {
+        expect(['P1 Wins!', 'P2 Wins!', 'Draw!']).to.include(msg);
+      });
   });
 });
